11e1dfe40be0f9e1cc81f049651f7e671ff4cb23
Marvoro53 <marvinlara321@gmail.com>
2020-12-03 21:48:33 -0600

    added the eleven exercises

---
+++ app.js
@@ -0,0 +1,45 @@
+var x= 40;
+var y= 2;
+var z= x+y;
+console.log(x+y);
+
+var number= 7*6;
+console.log(number);
+
+console.log(40+10-10+2);
+//2
+var food= "Mexican";
+console.log(food);
+console.log("Enfrijoladas " + "are my favorite " + food + " food");
+//3
+var egFamily= new Array('Julia', 'James', "Marvin");
+console.log(egFamily);
+//4
+console.log("All Strings are CrEaTeD equal" != "All STRINGS are CrEaTeD Equal");
+var boolean= "All Strings are CrEaTeD equal" != "All STRINGS are CrEaTeD Equal";
+console.log(boolean);
+//5
+var celcius= 12;
+var farenheit= celcius * 1.8 + 32;
+console.log(farenheit);
+//6
+var poem= "Blowing from the west" + "\nFallen leaves gather" + "\nIn the east";
+console.log(poem);
+//7
+var thingOne= "red";
+var thingTwo= "blue";
+console.log(thingOne, thingTwo);
+//8
+var fullName= "Marvin Eduardo Barron Lara";
+console.log(fullName);
+//9
+var bill= 10.25 + 3.99 + 7.15;
+var tip= bill * 0.15;
+var total= bill + tip;
+console.log("$",total);
+//10
+
+//11
+var big= "I'm BIG like an elephant";
+var small= "I'm small like a mouse";
+console.log(big, small);
